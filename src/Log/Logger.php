02c69c4203606864ac0d9897140659dd191e0ca8
Fabio Ribeiro <fabiorphp@gmail.com>
2015-04-21 00:15:04 -0300

    Logger
    * Improve Logger based on Monolog

---
+++ src/Log/Logger.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dafiti\Silex\Log;
+
+class Logger extends \Monolog\Logger
+{
+    public function log($message, $level = Logger::DEBUG, array $context = [])
+    {
+        return parent::log($level, $message, $context);
+    }
+
+    public function translateLevel($name)
+    {
+        if (is_int($name)) {
+            return $name;
+        }
+
+        $levels = $this->getLevels();
+        $upper = strtoupper($name);
+
+        if (!isset($levels[$upper])) {
+            throw new \InvalidArgumentException("Provided logging level '$name' does not exist. Must be a valid monolog logging level.");
+        }
+
+        return $levels[$upper];
+    }
+}
